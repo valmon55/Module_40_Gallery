44f35f8bae42fd201f888fe5211d8a161108dba2
valmon55 <fedor_ka@mail.ru>
2024-03-14 15:28:08 +0300

    Страница с ПИН готова!

--- XMR/XMR.Gallery/XMR.Gallery/Pages/MainPage.xaml.cs
+++ XMR/XMR.Gallery/XMR.Gallery/Pages/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using XMR.Gallery.Pages;
 
@@ -18,9 +19,38 @@ namespace XMR.Gallery
 
         private async void pin_Completed(object sender, EventArgs e)
         {
+            string pinCheck = string.Empty;
+            string enteredPin = (((Entry)sender).Text ?? string.Empty);
+
+            if (enteredPin.Length != 4)
+            {
+                await DisplayAlert("Ошибка", "ПИН должен быть из 4 знаков!", "Отмена");
+                return;
+            }
+
+            if (!Preferences.ContainsKey("PIN"))
+            {
+                Preferences.Set("PIN", enteredPin);
+            }
+            else 
+            {
+                pinCheck = Preferences.Get("PIN", "");
+
+                if (pinCheck != enteredPin)
+                {
+                    await DisplayAlert("Ошибка", "Неверный ПИН!", "Отмена");
+                    return;
+                }
+            }
             await Navigation.PushAsync(new PictureListPage("Галерея"));
 
             // скрываем введенный ПИН
+            ((Entry)sender).Text = string.Empty;
+        }
+
+        private void clearPin(object sender, EventArgs e)
+        {
+            Preferences.Clear();
             pin.Text = string.Empty;
         }
     }
