8f34d64ddfaae37ebe9291aff95e2655f5a5c8c2
valmon55 <fedor_ka@mail.ru>
2024-03-14 20:01:17 +0300

    Вопрос по открытию картинок

---
+++ XMR/XMR.Gallery/XMR.Gallery/Model/Picture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMR.Gallery.Model
+{
+    public class Picture
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public Picture(string path, string name) 
+        { 
+            Path = path;
+            Name = name;
+        }
+    }
+}
--- XMR/XMR.Gallery/XMR.Gallery/Pages/PictureListPage.xaml.cs
+++ XMR/XMR.Gallery/XMR.Gallery/Pages/PictureListPage.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XMR.Gallery.Model;
 
 namespace XMR.Gallery.Pages
 {
@@ -13,11 +15,62 @@ namespace XMR.Gallery.Pages
     public partial class PictureListPage : ContentPage
     {
         public static string PageName { get; set; }
+        public List<Picture> Pictures { get; set; } = new List<Picture>();
         public PictureListPage(string pageName)
         {
             PageName = pageName;
+            DriveInfo[] driveInfo = DriveInfo.GetDrives();
+            Pictures.Add(new Picture(driveInfo[0].Name, driveInfo[0].Name + " " + driveInfo[1].Name));
+            Pictures.Add(new Picture("/DCIM/Camera/20230504_153643.jpg", "Картинка 1"));
+            Pictures.Add(new Picture("VOLUME1\\DCIM\\Camera\\20230508_130949.jpg", "Картинка 2"));
+
+            Pictures.AddRange(GetAllPictures());
 
             InitializeComponent();
+
+            BindingContext = this;
+        }
+        private List<Picture> GetAllPictures()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            
+            List<Picture> list = new List<Picture>();
+
+            foreach(var drive in drives)
+            {
+                list.AddRange(GetAllDrivePicture(drive.RootDirectory,"jpg"));
+            }
+            return list;
+        }
+        private List<Picture> GetAllDrivePicture(DirectoryInfo directory, string filter)
+        {
+            List<Picture> list = new List<Picture>();
+            try
+            {
+                foreach (var dir in directory.GetDirectories())
+                {
+                    list.AddRange(GetDirFiles(dir, filter));
+
+                }
+            }
+            catch(Exception ex) { }
+            return list;
+        }
+        private List<Picture> GetDirFiles(DirectoryInfo directory, string filter)
+        {
+            List<Picture> list = new List<Picture>();
+            try
+            {
+                foreach (var file in directory.GetFiles())
+                {
+                    if (file.Name.Contains(filter))
+                    {
+                        list.Add(new Picture(directory.Name, file.Name));
+                    }
+                }
+            }
+            catch(Exception ex) { } 
+            return list;
         }
         private async void OpenPicture(object sender, EventArgs e)
         {
